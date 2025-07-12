b9e8dd63a0afe323a2ee1b033f9f37fb2723a47e
Tarun Chintakunta <tarunchintakunta@gmail.com>
2025-07-12 15:32:00 +0530

    Add ResumesPage component with styled-components; implement layout and content for resume management functionality, including Header and Footer integration.

---
+++ frontend/src/pages/ResumesPage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
+
+const PageContainer = styled.div`
+  padding-top: 80px;
+  min-height: 100vh;
+  background-color: ${props => props.theme.colors.background};
+`;
+
+const ContentContainer = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: ${props => props.theme.spacing.xl};
+`;
+
+const PageTitle = styled.h1`
+  margin-bottom: ${props => props.theme.spacing.lg};
+  color: ${props => props.theme.colors.text};
+`;
+
+const ResumesPage = () => {
+  return (
+    <>
+      <Header />
+      <PageContainer>
+        <ContentContainer>
+          <PageTitle>Resume Management</PageTitle>
+          <p>This page will allow you to upload, view, and manage candidate resumes.</p>
+        </ContentContainer>
+      </PageContainer>
+      <Footer />
+    </>
+  );
+};
+
+export default ResumesPage;
